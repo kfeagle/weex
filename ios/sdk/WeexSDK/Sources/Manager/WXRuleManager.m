df1f10400fadb42837399d71704a03a3a32c8fee
齐山 <sunjjbobo@163.com>
2017-01-05 11:36:14 +0800

    * [ios] bug fix: iconfont save tempsrc before download ,when download success,save to src

--- ios/sdk/WeexSDK/Sources/Manager/WXRuleManager.m
+++ ios/sdk/WeexSDK/Sources/Manager/WXRuleManager.m
@@ -83,7 +83,7 @@ static WXRuleManager *_sharedInstance = nil;
                 // if the fontSrc string is illegal, the fontURL will be nil
                 return;
             }
-            [fontFamily setObject:fontSrc forKey:@"src"];
+            [fontFamily setObject:fontSrc forKey:@"tempSrc"];
             [_fontStorage setObject:fontFamily forKey:rule[@"fontFamily"]];
             // remote font file
             NSString *fontfile = [NSString stringWithFormat:@"%@/%@",WX_FONT_DOWNLOAD_DIR,[WXUtility md5:[fontURL absoluteString]]];
@@ -97,6 +97,8 @@ static WXRuleManager *_sharedInstance = nil;
                 if (!error && url) {
                     // load success
                     NSMutableDictionary * dictForFontFamily = [weakSelf.fontStorage objectForKey:rule[@"fontFamily"]];
+                    NSString *fontSrc = [dictForFontFamily objectForKey:@"tempSrc"];
+                    [dictForFontFamily setObject:fontSrc forKey:@"tempSrc"];
                     [dictForFontFamily setObject:url forKey:@"localSrc"];
                 } else {
                     //there was some errors during loading
